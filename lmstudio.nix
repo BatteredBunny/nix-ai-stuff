d287bf6a36e7a4db7a3aff12045cba413cd562bc
ayes-web <ayes2022@protonmail.com>
2023-11-14 17:35:56 +0200

    Add mainProgram to lmstudio

--- lmstudio.nix
+++ lmstudio.nix
@@ -1,13 +1,25 @@
 {
   appimageTools,
   fetchurl,
+  lib,
 }:
-appimageTools.wrapType2 {
+appimageTools.wrapType2 rec {
   pname = "lmstudio";
   version = "0.2.8";
 
   src = fetchurl {
     url = "https://s3.amazonaws.com/releases.lmstudio.ai/prerelease/LM+Studio-0.2.8-beta-v1.AppImage";
     hash = "sha256-Ha1G9YrLrHBPYHoqOoMhqVr/9AtUQILCuxdp07kWebk=";
+  };
+
+  extraInstallCommands = ''
+    mv $out/bin/${pname}-${version} $out/bin/${pname}
+  '';
+
+  meta = with lib; {
+    description = "A GUI tool to run LLMs";
+    homepage = "https://lmstudio.ai/";
+    platforms = platforms.linux;
+    mainProgram = pname;
   };
 }
